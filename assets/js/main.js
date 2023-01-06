10fb923eae7c24f72e9602e1ef1660092f0297db
domieee <99103235+domieee@users.noreply.github.com>
2023-01-06 12:07:46 +0100

    grammar lol

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,13 @@
 function intro2(paramName) {
-        // Wir definieren unsere Funktion, und sagen ihr, dass sie einen Parameter entgegeben nehmen soll
-        // Die Variable die wir als Argument übergeben haben, wird als Parameter entgegen genommen.
+        // Wir definieren unsere Funktion, und sagen ihr dass sie einen Parameter entgegeben nehmen soll
+        // Die Variable die wir als Argument übergeben haben, wird als Parameter entgegengenommen.
         // Ohne Argument ist das Ergebnis 'undefinied'
 
     const varName = "SuperCoder"
         // Wir speichern einen String in einer Variable
 
     console.log("Hi, " + varName + ". Mein Name ist " + paramName + ".");
-    
-        // Und geben anschließen den Satz 'Hi, SuperCoder. Mein Name ist Dominik.' in der Konsole aus
+        // Und geben anschließend den Satz 'Hi, SuperCoder. Mein Name ist Dominik.' in der Konsole aus
 
 }
 
